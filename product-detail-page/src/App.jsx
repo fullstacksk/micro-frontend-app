37eac189e7681273fcc9eaba5c3df44f3d673228
Shailendra Kumar <fullstacksk@gmail.com>
2022-06-14 12:58:39 +0530

    tested synchronous loading

---
+++ product-detail-page/src/App.jsx
@@ -0,0 +1,26 @@
+import React, {Suspense, useState} from "react";
+import ReactDOM from "react-dom";
+import Footer from "home/Footer";
+const Header  = React.lazy( ()=> import ("home/Header"));
+
+import "./index.scss";
+
+const App = () => {
+  const [showHeader, setShowHeader] = useState(false);
+  return (
+  <div className="text-3xl mx-auto max-w-6xl">
+    {showHeader && <Suspense fallback={<div>Loading...</div>}>
+      <Header />
+    </Suspense>}
+      <button
+        className="p-5 bg-green-500 text-white"
+        onClick={()=> setShowHeader(!showHeader)}
+      >Show the Header</button>
+    <div className="py-10">
+      PDP page content
+    </div>
+    <Footer />
+  </div>
+)};
+
+ReactDOM.render(<App />, document.getElementById("app"));
