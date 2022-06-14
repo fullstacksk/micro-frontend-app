12b2355d54fb963bbbbd52dfc1e6406d8c380c76
Shailendra Kumar <fullstacksk@gmail.com>
2022-06-14 13:43:36 +0530

    removed lazy loading implementation

--- product-detail-page/src/App.jsx
+++ product-detail-page/src/App.jsx
@@ -1,26 +1,17 @@
-import React, {Suspense, useState} from "react";
+import React from "react";
 import ReactDOM from "react-dom";
 import Footer from "home/Footer";
-const Header  = React.lazy( ()=> import ("home/Header"));
+import Header from "home/Header";
 
 import "./index.scss";
 
-const App = () => {
-  const [showHeader, setShowHeader] = useState(false);
-  return (
+const App = () => (
   <div className="text-3xl mx-auto max-w-6xl">
-    {showHeader && <Suspense fallback={<div>Loading...</div>}>
-      <Header />
-    </Suspense>}
-      {!showHeader && <button
-        className="p-5 bg-green-500 text-white"
-        onClick={()=> setShowHeader(!showHeader)}
-      >Show the Header</button>}
+    <Header />
     <div className="py-10">
-      PDP page content
+      Home page content
     </div>
     <Footer />
   </div>
-)};
-
+);
 ReactDOM.render(<App />, document.getElementById("app"));
