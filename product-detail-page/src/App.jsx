5c0ae438fb272c8ec295b2f37a8da117aadf0d0f
Shailendra Kumar <fullstacksk@gmail.com>
2022-06-14 16:46:28 +0530

    added PDPContent component

--- product-detail-page/src/App.jsx
+++ product-detail-page/src/App.jsx
@@ -5,14 +5,15 @@ import Header from "home/Header";
 
 import "./index.scss";
 import SafeComponent from "./SafeComponent";
+import PDPContent from "./PDPContent";
 
 const App = () => (
   <div className="text-3xl mx-auto max-w-6xl">
     <SafeComponent>
-      <Header />
+      <Header app={{name:"PDP"}} />
     </SafeComponent>
     <div className="py-10">
-      Home page content
+      <PDPContent />
     </div>
     <Footer />
   </div>
